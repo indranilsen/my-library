2e92261c6bd1e228c107d4a48c05349d7126614c
Indranil Sen <indranilsen010@gmail.com>
2016-07-26 13:51:08 -0400

    Added tasks for custom css/js files and nodemon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.use(express.static('src/views'));
@@ -11,6 +11,6 @@ app.get('/', function(req, res) {
     res.send('Hello world!');
 });
 
-app.listen(5000, function(err){
+app.listen(port, function(err){
     console.log('Running server on port', port);
 });
