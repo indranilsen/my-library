0420a6325e2277d0d7058bf3d0a0fe12e96b948a
Indranil Sen <indranilsen010@gmail.com>
2016-07-27 10:20:15 -0400

    Added routing for books

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 var express = require('express');
-
 var app = express();
-
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
+
 
 app.use(express.static('public'));
+
 app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get(function(req, res) {
+        res.send('Books');
+    });
+
+bookRouter.route('/single')
+    .get(function(req, res) {
+        res.send('Single book');
+    });
+
+app.use('/Books', bookRouter)
+
 app.get('/', function(req, res) {
-    res.render('index', {title: "Hello from render", list:['a','b']});
+    res.render('index', {
+        title: "Hello from render", 
+        nav: [
+            {
+                Link: '/Books',
+                Text: 'Books'
+            },
+            {
+                Link: '/Authors',
+                Text: 'Authors'
+            }
+        ]
+    });
 });
 
 app.listen(port, function(err){
