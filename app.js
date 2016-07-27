6946ffa97ba583b1ac8d26996b5372e94fb454f4
Indranil Sen <indranilsen010@gmail.com>
2016-07-27 13:32:05 -0400

    Added book info to books.ejs

--- app.js
+++ app.js
@@ -1,23 +1,78 @@
+// Variables
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 var bookRouter = express.Router();
 
-
+// Serving static files
 app.use(express.static('public'));
 
-app.set('views', './src/views');
 
+// Setting up template engine
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+var books = [
+        {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+         {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+         {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+         {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+         {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+         {
+            title: 'Test Book',
+            genre: 'Fiction',
+            author: 'Test Author',
+            read: false
+        },
+    ];
+
+// Routes
 bookRouter.route('/')
     .get(function(req, res) {
-        res.send('Books');
+        res.render('books', {
+            title: "Hello from render", 
+            nav: [
+                {
+                    Link: '/Books',
+                    Text: 'Books'
+                },
+                {
+                    Link: '/Authors',
+                    Text: 'Authors'
+                }
+            ],
+            books: books
+        });
     });
 
 bookRouter.route('/single')
     .get(function(req, res) {
-        res.send('Single book');
+        res.send('d book');
     });
 
 app.use('/Books', bookRouter)
@@ -38,6 +93,8 @@ app.get('/', function(req, res) {
     });
 });
 
+
+// Starting server
 app.listen(port, function(err){
     console.log('Running server on port', port);
 });
