cb0459d07a1b88a95117b990201bfa6bc5ca0a2a
Indranil Sen <indranilsen010@gmail.com>
2016-07-30 15:15:38 -0400

    Sign in feature added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var passport = require('passport');
 
 var router = function (nav) {
     authRouter.route('/signUp')
@@ -20,6 +21,13 @@ var router = function (nav) {
                     });
                 });
             });
+        });
+    
+    authRouter.route('/signIn')
+        .post(passport.authenticate('local', {
+            failureRedirect: '/'
+        }), function(req, res){
+            res.redirect('/auth/profile');
         });
     
     authRouter.route('/profile')
